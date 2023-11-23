b9cd1ba5b5c85863c8d5261e064316d7e04ea681
anatoli dyrul <dyrulan@gmail.com>
2023-11-23 22:02:21 +0300

    7.5 Вызов метода с возвращающим типом

--- src/main/java/ru/oop/DummyDic.java
+++ src/main/java/ru/oop/DummyDic.java
@@ -1,13 +1,12 @@
 package ru.oop;
 
 public class DummyDic {
-   public String engToRus(String eng) {
-       String word = "Неизвестное слово. " + eng;
-       return word;
-   }
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng;
+    }
 
     public static void main(String[] args) {
-    DummyDic word = new DummyDic();
+        DummyDic word = new DummyDic();
         System.out.println(word.engToRus("Dog"));
     }
 }
