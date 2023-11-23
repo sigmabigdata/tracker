48b1076008bb673dbce3c8e58337b9d0966908dc
anatoli dyrul <dyrulan@gmail.com>
2023-11-23 22:10:46 +0300

    7.6 Поля объекта

--- src/main/java/ru/oop/Cat.java
+++ src/main/java/ru/oop/Cat.java
@@ -3,19 +3,17 @@ package ru.oop;
 public class Cat {
 
     private String food;
+    private String name;
 
     public void show() {
-        System.out.println(this.food);
-        System.out.println(this.name);
+        System.out.println("Кличка котика " + this.name + ". Он ел " + this.food);
     }
 
     public void eat(String meat) {
         this.food = meat;
     }
 
-    private String name;
-
-     public void giveNick(String nick) {
+    public void giveNick(String nick) {
         this.name = nick;
     }
 
