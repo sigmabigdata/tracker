2e1e97512cfdefe9b51641fd27bec9755bda310f
Sigmabigdata <sigmabigdata@gmail.com>
2023-12-05 09:15:35 +0300

    9.2 Модель данных

--- src/main/java/ru/pojo/College.java
+++ src/main/java/ru/pojo/College.java
@@ -1,13 +1,14 @@
 package ru.pojo;
 
-public class College extends Student {
+public class College {
 
     public static void main(String[] args) {
-        College student = new College();
+        Student student = new Student();
         student.setStudentName("Иванов Иван");
         student.setGroup(115698);
         student.setYear(2018);
 
-        System.out.println("Студент " + student.getStudentName() + " из группы " + student.getGroup() + " поступил в " + student.getYear() + " году.");
+        System.out.println("Студент " + student.getStudentName() + " из группы "
+                + student.getGroup() + " поступил в " + student.getYear() + " году.");
     }
 }
