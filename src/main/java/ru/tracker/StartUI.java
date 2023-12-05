f978fe8d9b09949d7ca4332446b0d3f3ad260b3d
Sigmabigdata <sigmabigdata@gmail.com>
2023-12-05 08:38:59 +0300

    8.7 Date. Отображение даты

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -3,12 +3,10 @@ package ru.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class StartUI extends Item {
+public class StartUI {
 
     public static void main(String[] args) {
-        StartUI item = new StartUI();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        LocalDateTime currentDateTime = item.getCreated();
-        System.out.println(currentDateTime.format(formatter));
+        Item item = new Item();
+        System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
     }
 }
