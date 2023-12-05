d2c0aba48ffe487bf0b6a6a64b4aa895e1d34008
Sigmabigdata <sigmabigdata@gmail.com>
2023-12-05 13:38:05 +0300

    9.5 Удаление моделей из массива

---
+++ src/main/java/ru/pojo/ShopDrop.java
@@ -0,0 +1,13 @@
+package ru.pojo;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        for (int i = index + 1; i < products.length; i++) {
+        products[i - 1] = products[i];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+}
+
+
