89e41bd42a39653cb64a939ae21911df754c2a63
Sigmabigdata <sigmabigdata@gmail.com>
2023-12-08 16:50:34 +0300

    9.7 Tracker - хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,32 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name != null) {
+                rsl[index] = name;
+                size++;
+            }
+        }
+        rsl = Arrays.copyOf(items, size);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] copyItems = new Item[items.length];
+        int i = 0;
+        for (int index = 0; index < size; index++) {
+            Item name = items[index];
+            if (name.getName().equals(key)) {
+                copyItems[index] = name;
+                i++;
+            }
+        }
+        copyItems = Arrays.copyOf(items, i);
+        return copyItems;
     }
 }
