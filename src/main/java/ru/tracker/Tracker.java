67d382e11afc94a3aa406f444ace5fef1bd69e47
Sigmabigdata <sigmabigdata@gmail.com>
2023-12-13 15:11:52 +0300

    9.8 Метод замены заявки. Tracker.replace

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -40,5 +33,27 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(copyItems, i);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+
+        int indexId = indexOf(id);
+        if (indexId != -1) {
+            item.setId(id);
+            items[indexId] = item;
+            return true;
+        }
+        return false;
     }
 }
