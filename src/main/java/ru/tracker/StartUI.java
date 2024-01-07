55aa5cac6526910d44156fcd60a2fec672756d17
anatoli dyrul <dyrulan@gmail.com>
2024-01-07 22:43:17 +0300

    10.6 StartUI. Вывод всех заявок

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.tracker;
 import java.util.Scanner;
 
 public class StartUI {
+
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
@@ -16,6 +17,16 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
+            } else if (select == 1) {
+                System.out.println("=== Вывод всех заявок ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
+                }
             } else if (select == 6) {
                 run = false;
             }
