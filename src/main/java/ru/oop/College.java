7197216560e92ad1fb7b3010d2d220e99e5e79f1
anatoli dyrul <dyrulan@gmail.com>
2024-01-10 19:45:37 +0300

    10.14 Приведение типов

---
+++ src/main/java/ru/oop/College.java
@@ -0,0 +1,9 @@
+package ru.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman fr = new Freshman();
+        Student st = fr;
+        Object obj = fr;
+    }
+}
