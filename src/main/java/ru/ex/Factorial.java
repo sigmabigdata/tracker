63f1bd285fa1b5f0ea1d7f1ab38f1432fe091fb8
Sigmabigdata <sigmabigdata@gmail.com>
2024-01-12 16:21:57 +0300

    11.3 throw new RuntimeException

---
+++ src/main/java/ru/ex/Factorial.java
@@ -0,0 +1,18 @@
+package ru.ex;
+
+public class Factorial {
+    public int calc(int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("Введенное значение меньше 1");
+        }
+        int result = 1;
+        for (int index = 2; index <= number; index++) {
+            result *= index;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Factorial().calc(-1));
+    }
+}
