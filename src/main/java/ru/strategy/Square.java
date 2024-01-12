1c802b5bdf1162a9d8df4b66eede90a222dc2322
Sigmabigdata <sigmabigdata@gmail.com>
2024-01-12 09:20:18 +0300

    10_22_Шаблон_проектирования_Стратегия

---
+++ src/main/java/ru/strategy/Square.java
@@ -0,0 +1,12 @@
+package ru.strategy;
+
+public class Square implements Shape {
+
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    "  ---- " + ln
+                + " |    | " + ln
+                + " |____|" + ln;
+    }
+}
