fc1e5c98efba971c188cbe53dcfe8bd4d8eac417
Sigmabigdata <sigmabigdata@gmail.com>
2024-01-18 11:32:01 +0300

    11_13_Принципы_раннего_возврата_и_охранных_выражений

---
+++ src/main/java/ru/early/PasswordValidator.java
@@ -0,0 +1,64 @@
+package ru.early;
+
+public class PasswordValidator {
+    private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
+
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException(
+                    "Password can't be null"
+            );
+        }
+
+        boolean hasUpCase = false;
+        boolean hasLowCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException(
+                    "Password should be length [8, 32]"
+            );
+        }
+        for (char symbol : password.toCharArray()) {
+            if (Character.isUpperCase(symbol)) {
+                hasUpCase = true;
+            } else if (Character.isLowerCase(symbol)) {
+                hasLowCase = true;
+            } else if (Character.isDigit(symbol)) {
+                hasDigit = true;
+            } else if (!Character.isLetterOrDigit(symbol)) {
+                hasSpecial = true;
+            }
+        }
+
+        if (!hasUpCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one uppercase letter"
+            );
+        }
+        if (!hasLowCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one lowercase letter"
+            );
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one figure"
+            );
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one special symbol"
+            );
+        }
+        for (String forbidden : FORBIDDEN) {
+            if (password.toLowerCase().contains(forbidden)) {
+                throw new IllegalArgumentException(
+                        "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
+                );
+            }
+        }
+        return password;
+    }
+}
