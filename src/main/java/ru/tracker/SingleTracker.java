526fb8bca49f103793d0254b4c3ff53d46aade68
Sigmabigdata <sigmabigdata@gmail.com>
2024-01-19 11:39:08 +0300

    12.2 Singleton

---
+++ src/main/java/ru/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.tracker;
+
+public final class SingleTracker {
+
+    private Tracker tracker = new Tracker();
+    private static SingleTracker instance;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String name) {
+        return tracker.findByName(name);
+    }
+
+    public boolean replace(String id, Item item) {
+        return tracker.replace(item.getId(), item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+}
