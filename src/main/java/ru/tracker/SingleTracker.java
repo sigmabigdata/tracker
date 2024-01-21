868aa2ebea228ca62f67975eb3f90fd4451d5523
anatoli dyrul <dyrulan@gmail.com>
2024-01-21 22:34:52 +0300

    12.2

--- src/main/java/ru/tracker/SingleTracker.java
+++ src/main/java/ru/tracker/SingleTracker.java
@@ -20,7 +20,8 @@ public final class SingleTracker {
     }
 
     public Item findById(int id) {
-        return null;
+
+        return tracker.findById(id);
     }
 
     public Item[] findAll() {
