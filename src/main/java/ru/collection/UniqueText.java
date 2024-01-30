2140f64a36f4eaac5e4151d0462321c25e70f5bf
anatoli dyrul <dyrulan@gmail.com>
2024-01-30 22:04:36 +0300

    14.3 Идентичные тесты

---
+++ src/main/java/ru/collection/UniqueText.java
@@ -0,0 +1,26 @@
+package ru.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+
+        for (String word : text) {
+            if (!check.contains(word)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
