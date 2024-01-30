2262991220ccdae6add006d3d0ff1dbd038a34a9
Anatoliy Dyrul <dyrulan@gmail.com>
2024-01-30 13:58:40 +0300

    14.2 Уникальные задачи

---
+++ src/main/java/ru/collection/FullSearch.java
@@ -0,0 +1,14 @@
+package ru.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public HashSet<String> extractNumber(List<Task> tasks) {
+        HashSet<String> numbers = new HashSet<>();
+        for (Task task : tasks) {
+            numbers.add(task.getNumber());
+        }
+        return numbers;
+    }
+}
