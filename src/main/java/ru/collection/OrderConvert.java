dd7ad6107732dd6beef7f21ccecb4a96c8209cc4
anatoli dyrul <dyrulan@gmail.com>
2024-02-03 22:14:19 +0300

    16.3 Преобразования

---
+++ src/main/java/ru/collection/OrderConvert.java
@@ -0,0 +1,14 @@
+package ru.collection;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class OrderConvert {
+    public static HashMap<String, Order> process(List<Order> orders) {
+        HashMap<String, Order> map = new HashMap<>();
+        for (Order element : orders) {
+            map.put(element.getNumber(), element);
+        }
+        return map;
+    }
+}
