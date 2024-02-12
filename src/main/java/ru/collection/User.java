fb9f8bba8354b93bfb7378a03c237c6271df5b25
Anatoliy Dyrul <dyrulan@gmail.com>
2024-02-12 15:24:40 +0300

    17.2 Организовать сортировку User

---
+++ src/main/java/ru/collection/User.java
@@ -0,0 +1,50 @@
+package ru.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int nameCompare = name.compareTo(o.name);
+        if (nameCompare != 0) {
+            return nameCompare;
+        } else {
+            return Integer.compare(age, o.age);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age
+                && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
